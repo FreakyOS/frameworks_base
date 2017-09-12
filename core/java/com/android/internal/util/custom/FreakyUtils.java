3b8b49612b7ed7331435213c50fc16b0b309b433
ezio84 <brabus84@gmail.com>
2017-09-12 14:20:27 +0800

    Add partial/full screenshot QS tile
    single tap to choose the mode, long press to fire
    thanks kantjer for the icons!
    
    Change-Id: I64415c8d05cba7433f421c59178ea9c08d4d4d2f
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- core/java/com/android/internal/util/custom/FreakyUtils.java
+++ core/java/com/android/internal/util/custom/FreakyUtils.java
@@ -51,6 +51,9 @@ public class FreakyUtils {
     public static final String INTENT_SCREENSHOT = "action_take_screenshot";
     public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
     private static IStatusBarService mStatusBarService = null;
+    
+    public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
 
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
@@ -169,6 +172,15 @@ public class FreakyUtils {
                         ServiceManager.getService("statusbar"));
             }
             return mStatusBarService;
+        }
+    }
+    
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 
