bec20fbd8764421d68ab1b2fe55b68e1cb8de9d4
maxwen <max.weninger@gmail.com>
2018-05-23 21:55:04 +0200

    base: SystemUI: prevent that double tap to sleep triggers panel expand
    Change-Id: I5223580f21a10b4e419ed6af9d6b89d569280662
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -60,7 +60,7 @@ import android.widget.FrameLayout;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.internal.util.custom.Utils;
+import com.android.internal.util.custom.FreakyUtils;
 import com.android.keyguard.KeyguardClockSwitch;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.keyguard.KeyguardUpdateMonitor;
@@ -492,7 +492,20 @@ public class NotificationPanelView extends PanelView implements
         mDoubleTapGesture = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent e) {
-                Utils.switchScreenOff(context);
+                FreakyUtils.switchScreenOff(context);
+                // quick pulldown can trigger those values
+                // on double tap - so reset them
+                mQsExpandImmediate = false;
+                requestPanelHeightUpdate();
+                setListening(false);
+                return true;
+            }
+        });
+        mLockscreenDoubleTapToSleep = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                FreakyUtils.switchScreenOff(context);
                 return true;
             }
         });
