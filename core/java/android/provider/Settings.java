34de27b236a9bed718ac22f6157b0dc4a360b063
SKULSHADY <anushekprasal@gmail.com>
2018-12-30 11:57:01 +0700

    base: Switch styles [1/3]
    Add back from Pie
    
    Change-Id: Ie4dad5a259e28bbab91bab8e3a69c16b55a1171f
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5197,6 +5197,11 @@ public final class Settings {
         public static final String VIBRATE_ON_NOTIFICATIONS = "vibrate_on_notifications";
 
         /**
+         * @hide
+         */
+        public static final String SWITCH_STYLE = "switch_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
