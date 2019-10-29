61d321d32cfb7ff25c7e5665816c07e39a23470b
Carlo Savignano <carlosavignano@aospa.co>
2019-10-29 23:49:36 +0100

    core: Pocket: policy: introduce pocket lock
    * Block touch screen and keys inputs when device is in pocket.
    * Safe-door to disable pocket lock within long press power button
      if sensors do not behave correctly after PocketManager.isDeviceInPocket()
      returns true.
    * Window uses TYPE_SYSTEM_ERROR flag with ensure high
      priority over the lockscreen and other high priority windows.
    * Do not illuminate buttons when pocket lock is showing.
    * Disable fingerprint events and gestures when in pocket
    * Be friendly towards Keypress Boost
    
    * Fix  booting issues
    * Fix  NPEs
    * Bring to Android 10
    
    Change-Id: I34108fa38f28f15b0b99c8ebb6551dcc4dabad14
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -330,6 +330,8 @@ public final class SystemServer {
     private Future<?> mSensorServiceStart;
     private Future<?> mZygotePreload;
 
+    public boolean safeMode = false;
+
     private class AdbPortObserver extends ContentObserver {
         public AdbPortObserver() {
             super(null);
@@ -346,6 +348,7 @@ public final class SystemServer {
             }
         }
     }
+
 
     /**
      * Start the sensor service. This is a blocking call and can take time.
@@ -1113,7 +1116,10 @@ public final class SystemServer {
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
-        final boolean safeMode = wm.detectSafeMode();
+
+        if(wm != null) {
+            safeMode = wm.detectSafeMode();
+        }
         if (safeMode) {
             // If yes, immediately turn on the global setting for airplane mode.
             // Note that this does not send broadcasts at this stage because
@@ -1843,6 +1849,7 @@ public final class SystemServer {
 
             traceBeginAndSlog("StartCrossProfileAppsService");
             mSystemServiceManager.startService(CrossProfileAppsService.class);
+            traceEnd();
 
             traceBeginAndSlog("StartPocketService");
             mSystemServiceManager.startService(PocketService.class);
