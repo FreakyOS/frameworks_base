e9684c915a6739dd8a0e3b0ae9d8aa520db481d3
Marko Man <darkobas@gmail.com>
2019-11-27 23:19:08 +0100

    overlay: Introduce skin tones
    Change-Id: Icea780d880b5c38598e5d8dff07134fea8e85e4e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -93,6 +93,20 @@ LOCAL_REQUIRED_MODULES := \
         IconShapePebbleOverlay \
         IconShapeTaperedRectOverlay \
         IconShapeVesselOverlay \
+        PrimaryColorOneplusDarkOverlay \
+        PrimaryColorAOSiPNatureOverlay \
+        PrimaryColorAOSiPOceanOverlay \
+        PrimaryColorCharcoalBlackOverlay \
+        PrimaryColorLeadBlackOverlay \
+        PrimaryEtherealOverlay \
+        PrimaryColorDerpClearOverlay \
+        PrimaryColorSolarizedDarkOverlay \
+        PrimaryFlameOverlay \
+        PrimaryColorDarkBlueOverlay \
+        PrimaryColorEyeSootherOverlay \
+        PrimaryColorBlissClearOverlay \
+        PrimaryColorBlissBlackOverlay \
+        PrimaryEthereal2Overlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
