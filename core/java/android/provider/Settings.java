084fef5a23c9bc1daf209dfdab63c10371ec580b
Ankit-Gourav <Ankitgourav035@gmail.com>
2019-12-25 12:17:06 +0700

    Disable Notifications vibration [1/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>
    Change-Id: I76c36d808c72558a2588a2b318cc89ae0453e4bb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5183,6 +5183,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * Whether to vibrate on notifications
+         * @hide
+         */
+        public static final String VIBRATE_ON_NOTIFICATIONS = "vibrate_on_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
