e04235dbaee1159255ac714a0e45c6e14f9a8d0a
Anushek Prasal <anushekprasal@gmail.com>
2020-02-22 13:20:44 +0530

    base: Add back gesture haptic feedback toggle [1/2]
    Use config_vibrateOnIconAnimation to detect whether the device is capable of subtle vibrations and use EFFECT_TICK for those devices
    
    Change-Id: I274a26f9ed4b71889ebb0acc7b02f73662a52a6a
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -857,6 +857,10 @@ public class NavigationBarView extends FrameLayout implements
         mEdgeBackGestureHandler.onSettingsChanged();
     }
 
+   public void updateBackGestureHaptic() {
+        mEdgeBackGestureHandler.setStateForBackGestureHaptic();
+    }
+
     public void setAccessibilityButtonState(final boolean visible, final boolean longClickable) {
         mLongClickableAccessibilityButton = longClickable;
         getAccessibilityButton().setLongClickable(longClickable);
