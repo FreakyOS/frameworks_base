db19a90f91fa3f5452f0373f4b69ad380b6a560f
Marko Man <darkobas@gmail.com>
2020-02-26 22:00:02 +0530

    SystemUI: FPS Info Overlay & Tile
    ** SDB - This commit is amalgam of ~
    1) CPU Info - c93c75f
    2) FPS Device Info - DerpFest-Devices/device_oneplus_msm8998-common@e6ec51c#diff-195d2c91fafb0c372a1d6dc13d2bba4e
    
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>
    Change-Id: I65a4a1a330721878decc89164e2efb68f6533b8b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14734,6 +14734,11 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
+
+        /**
          * The TCP/IP port to run ADB on, or 0 for USB
          * @hide
          */
