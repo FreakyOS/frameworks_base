68d554ecdc2dc08d32c1b7d2d819a49aaa4ffe35
bunnyyTheFreak <hsinghalk@yahoo.com>
2020-07-23 11:02:49 +0000

    core: FreakyUtils: add isPackageAvailabe method with interchanging the passing arguments
    Change-Id: I72b09f1b257030d9ee5d51af2b1c0d97e80a3ec4
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- core/java/com/android/internal/util/custom/FreakyUtils.java
+++ core/java/com/android/internal/util/custom/FreakyUtils.java
@@ -93,6 +93,19 @@ public class FreakyUtils {
         }
     }
 
+    public static boolean isPackageAvailableMethod(Context context, String packageName) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
